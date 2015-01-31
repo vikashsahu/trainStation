eb531ea33c1e794252a02401d015d612de32fd19
Vikash <vsahu24@gmail.com>
2015-01-31 09:39:03 -0500

    fix table centering issue

--- trainStation.js
+++ trainStation.js
@@ -21,24 +21,25 @@ function dispTrains() {
 function summarize() {
 	//No trains today
 	if (times[0].indexOf(moment().format("dddd"))===-1) { 
-		document.getElementById("turnstile").innerHTML = "No trains today -- check back tomorrow!";
+		document.getElementById("cryptic").innerHTML = "No trains today -- check back tomorrow!";
+		document.getElementById("clarity").innerHTML = "(that means no office hours today)";
 	}	
 	else {//it's a day when office hours are held
 		if (moment().hour() < times[1][0]) {//too early
-			document.getElementById("turnstile").innerHTML = "You're early! The trains haven't arrived yet.";
-			document.getElementById('turnstile').innerHTML += "(that means I'm not in my office yet)";
+			document.getElementById("cryptic").innerHTML = "You're early! The trains haven't arrived yet.";
+			document.getElementById('clarity').innerHTML += "(that means I'm not in my office yet)";
 		}
 		else if (moment().hour() >= times[2][0]){ //too late
 			document.getElementById("cryptic").innerHTML = "The trains have departed already!";
-			document.getElementById("clarity").innerHTML += "<br />(that means you've missed office hours)";
+			document.getElementById("clarity").innerHTML += "(that means you've missed office hours)";
 		}
 		else {
 
 			var boardStr = "Morrill Hall";
 			var boardStrLink = boardStr.link("http://www.umd.edu/CampusMaps/bld_detail.cfm?bld_code=MOR");
 
-			document.getElementById("turnstile").innerHTML = "The trains are currently boarding! Catch Vikash at 1102 " + boardStrLink + ".";
-			document.getElementById("turnstile").innerHTML += "(that means I'm in my office)";
+			document.getElementById("cryptic").innerHTML = "The trains are currently boarding! Catch Vikash at 1102 " + boardStrLink + ".";
+			document.getElementById("clarity").innerHTML += "(that means I'm in my office)";
 		}
 	}
 }
