3b5d4fe52c40b910a9b9f21e5c73802fada7b5c4
Vikash <vsahu24@gmail.com>
2015-09-10 21:40:23 -0400

    updated for 2015-16 school year

--- trainStation.js
+++ trainStation.js
@@ -1,5 +1,5 @@
 
-var times = [["Monday","Wednesday"],[11,11],[13,13]];
+var times = [["Tuesday","Thursday"],[1230,1230],[1330,1330]];
 
 function weekStartDay() {
 	var startOfWeek = moment().startOf('isoWeek').format("MMMM Do");
@@ -17,7 +17,7 @@ function dispTrains() {
 
 	for (var i=1, row; row=getTimeTable.rows[i]; i++) {
 		for (var j=0, col; col=row.cells[j]; j++) {
-			getTimeTable.rows[i].cells[j].innerHTML = times[i][j] + "-" + (times[i+1][j]-12);
+			getTimeTable.rows[i].cells[j].innerHTML = times[i][j] + "-" + (times[i+1][j]-1200);
 		}
 	}
 }
@@ -29,12 +29,14 @@ function summarize() {
 		document.getElementById("clarity").innerHTML = "(that means no office hours today)";
 	}	
 	else {//it's a day when office hours are held
-		if (moment().hour() < times[1][0]) {//too early
+		if (moment().hour() < times[1][0]/100) {//too early
+			console.log(times[1][0]);
+			console.log(moment().hour());
 			document.getElementById("cryptic").innerHTML = "You're early! The trains haven't arrived yet.";
 			document.getElementById("clarity").innerHTML = "(that means I'm not in my office yet)";
 			$("body").css("background-image", "url(no_train_animated.gif)");
 		}
-		else if (moment().hour() >= times[2][0]){ //too late
+		else if (moment().hour() >= times[2][0]/100){ //too late
 			document.getElementById("cryptic").innerHTML = "The trains have departed already!";
 			document.getElementById("clarity").innerHTML = "(that means you've missed office hours)";
 			$("body").css("background-image", "url(depart_animated.gif)");
@@ -45,10 +47,10 @@ function summarize() {
 				}, 10550);
 		}
 		else {
-			var boardStr = "Morrill Hall";
-			var boardStrLink = boardStr.link("http://www.umd.edu/CampusMaps/bld_detail.cfm?bld_code=MOR");
+			var boardStr = "A.V. Williams";
+			var boardStrLink = boardStr.link("http://www.umd.edu/CampusMaps/bld_detail.cfm?bld_code=AVW");
 
-			document.getElementById("cryptic").innerHTML = "The trains are currently boarding! Catch Vikash at 1102 " + boardStrLink + ".";
+			document.getElementById("cryptic").innerHTML = "The trains are currently boarding! Catch Vikash at 4103 " + boardStrLink + ".";
 			document.getElementById("clarity").innerHTML = "(that means I'm in my office)";
 			$("body").css("background-image", "url(arrive_animation_stop.gif)");
 		}
